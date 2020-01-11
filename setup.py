d6dbbdc0feb127b1fef6b73c7af74f6e36037a98
PeteRoyAlex <59569764+PeteRoyAlex@users.noreply.github.com>
2020-01-11 19:31:03 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sotd_modules',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/PeteRoyAlex/sotd_modules',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['sotd_modules'],
+      zip_safe=False)
